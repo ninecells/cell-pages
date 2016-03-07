b178d409c31045cfb13c00cf7e0f4c7ff7a8b69c
koojunho <koojh@9cells.com>
2016-03-07 16:26:19 +0900

    Wiki 제거

--- PagesServiceProvider.php
+++ PagesServiceProvider.php
@@ -5,7 +5,6 @@ namespace NineCells\Pages;
 use App;
 use Illuminate\Support\ServiceProvider;
 use NineCells\Admin\PackageList;
-use NineCells\Wiki\WikiServiceProvider;
 
 class PagesServiceProvider extends ServiceProvider
 {
@@ -13,13 +12,14 @@ class PagesServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'ncells');
 
+        /*
         $packages->addPackageInfo('wiki', 'Wiki', function() {
             return 'WikiServiceProvider.php를 수정하세요';
         });
+        */
     }
 
     public function register()
     {
-        App::register(WikiServiceProvider::class);
     }
 }
