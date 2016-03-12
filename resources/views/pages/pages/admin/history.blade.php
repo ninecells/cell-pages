fed4f3b49887d3dcce1601ee8228176047d709b9
koojunho <koojh@9cells.com>
2016-03-12 17:13:00 +0900

    히스토리 페이지의 테이블 스타일 수정

--- resources/views/pages/pages/admin/history.blade.php
+++ resources/views/pages/pages/admin/history.blade.php
@@ -8,26 +8,41 @@
 <p>
     <a id="btn-compare" href="#" class="btn btn-success" data-page-slug="{{ $page->slug }}">비교하기</a>
 </p>
+<style>
+    table .title {
+        width: 100%;
+    }
+
+    table .rev,.left,.right,.created,.writer {
+        width: auto;
+        white-space: nowrap;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+</style>
 <table class="table table-bordered">
     <thead>
     <tr>
-        <th>L</th>
-        <th>R</th>
-        <th>#</th>
-        <th>제목</th>
-        <th>변경</th>
-        <th>변경한 사람</th>
+        <th class="rev">#</th>
+        <th class="left">L</th>
+        <th class="right">R</th>
+        <th class="title">제목</th>
+        <th class="created">변경</th>
+        <th class="writer">변경한 사람</th>
     </tr>
     </thead>
     <tbody>
     @foreach ( $histories as $history )
     <tr>
-        <td><input name="left" type="radio" value="{{ $history->rev }}"/></td>
-        <td><input name="right" type="radio" value="{{ $history->rev }}"/></td>
-        <td>{{ $history->rev }}</td>
-        <td><a href="/admin/pages/{{ $history->slug }}/{{ $history->rev }}">{{ $history->title }}</a></td>
-        <td>{{ $history->created_at->diffForHumans() }}</td>
-        <td><a href="/members/{{ $history->writer_id }}">{{ $history->writer->name }}</a></td>
+        <td class="rev">{{ $history->rev }}</td>
+        <td class="left"><input name="left" type="radio" value="{{ $history->rev }}"/></td>
+        <td class="right"><input name="right" type="radio" value="{{ $history->rev }}"/></td>
+        <td class="title"><a href="/admin/pages/{{ $history->slug }}/{{ $history->rev }}">{{ $history->title }}</a></td>
+        <td class="created">{{ $history->created_at->diffForHumans() }}</td>
+        <td class="writer"><a href="/members/{{ $history->writer_id }}">{{ $history->writer->name }}</a></td>
     </tr>
     @endforeach
     </tbody>
