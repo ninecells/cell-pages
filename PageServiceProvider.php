32a375163c3c25aa5401eecb863ed82f123b138b
koojunho <koojh@9cells.com>
2016-03-16 01:03:02 +0900

    AdminManager 를 사용하여 admin 정보 저장소를 선택할 수 있게 함

--- PageServiceProvider.php
+++ PageServiceProvider.php
@@ -5,18 +5,18 @@ namespace NineCells\Pages;
 use App;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Support\ServiceProvider;
-use NineCells\Admin\AdminHelper;
+use NineCells\Admin\AdminManager;
 use NineCells\Admin\AdminServiceProvider;
 use NineCells\Admin\PackageList;
 use NineCells\Auth\AuthServiceProvider;
 
 class PageServiceProvider extends ServiceProvider
 {
-    private function registerPolicies(GateContract $gate)
+    private function registerPolicies(GateContract $gate, AdminManager $admin)
     {
-        $gate->before(function ($user, $ability) {
+        $gate->before(function ($user, $ability) use ($admin) {
             if ($ability === "page-admin") {
-                if ($user && AdminHelper::isAdmin($user)) {
+                if ($user && $admin->isAdmin($user)) {
                     return $user;
                 }
             } else if ($ability === "page-write") {
@@ -25,9 +25,9 @@ class PageServiceProvider extends ServiceProvider
         });
     }
 
-    public function boot(GateContract $gate, PackageList $packages)
+    public function boot(GateContract $gate, AdminManager $admin, PackageList $packages)
     {
-        $this->registerPolicies($gate);
+        $this->registerPolicies($gate, $admin);
 
         if (!$this->app->routesAreCached()) {
             require __DIR__ . '/Http/routes.php';
